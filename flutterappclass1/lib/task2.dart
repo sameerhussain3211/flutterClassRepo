b6ffc21dd1fc534dd00920a861027a28fff660a3
sameerhussain3211 <sameerhussain44332211@gmail.com>
2024-02-17 15:25:55 +0500

    task1codeLab1

--- flutterappclass1/lib/task2.dart
+++ flutterappclass1/lib/task2.dart
@@ -32,111 +32,82 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool passwordVisible = false;
-
-  @override
-  void initState() {
-    super.initState();
-    passwordVisible = true;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Login"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 70,
-                  child: Image.asset('assects/images/download.png'),
-                ),
-              ],
-            ),
-            Container(
-              height: 30,
-            ),
-            SizedBox(
-              width: 400,
-              child: TextField(
-                // obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter Email'),
+     
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 50,
               ),
-            ),
-            Container(
-              height: 30,
-            ),
-            Container(
-              width: 400,
-              child: TextField(
-                obscureText: passwordVisible,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                  hintText: "Enter Password",
-
-                  suffixIcon: IconButton(
-                    icon: Icon(passwordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off),
-                    onPressed: () {
-                      setState(
-                        () {
-                          passwordVisible = !passwordVisible;
-                        },
-                      );
-                    },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 70,
+                    child: Image.asset('assects/images/download.png'),
                   ),
-
-                  // filled: true,
-                ),
-                keyboardType: TextInputType.visiblePassword,
-                textInputAction: TextInputAction.done,
+                ],
               ),
-            ),
-            Container(
-              height: 50,
-            ),
-            Text("No Account? create an account "),
-            Container(
-              height: 10,
-            ),
-            InkWell(
-              onTap: () {
-                print("hello");
-              },
-              child: Container(
-                child: Text(
-                  "forget Password",
-                  style: TextStyle(
-                    color: Colors.blue,
+              const SizedBox(height: 16.0),
+              const Text(
+                'SHRINE',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Container(
+                height: 100,
+              ),
+             const SizedBox(
+                width: 400,
+                child: TextField(
+                  // obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      labelText: 'Email',
+                      hintText: 'Enter Email'),
+                ),
+              ),
+              Container(
+                height: 12,
+              ),
+            const  SizedBox(
+                width: 400,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      filled: true,
+                      
+                      hintText: 'Enter Password'),
+                ),
+              ),
+              Container(
+                height: 50,
+              ),
+              OverflowBar(
+                alignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    child: Text("CANCEL"),
+                    onPressed: (){
+                    print("hello");
+                  },
                   ),
-                ),
-              ),
-            )
-          ],
+                  const SizedBox(width: 50,),
+                  ElevatedButton(onPressed: (){print("hello");}, child: Text("Next"))
+        
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton.extended(
-              onPressed: () {
-                print("hello");
-              },
-              label: Text("login"))
-        ],
       ),
     );
   }
