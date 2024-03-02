43b3980f55c9c5185d069a8ecb470dd50710eae2
sameerhussain3211 <sameerhussain44332211@gmail.com>
2024-03-02 16:52:43 +0500

    moke Test

---
+++ mocktext/android/app/src/main/kotlin/com/example/mocktext/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mocktext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mocktext/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mocktext/Home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: viewlist());
+  }
+}
